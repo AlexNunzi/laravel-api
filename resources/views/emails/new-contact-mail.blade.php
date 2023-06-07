ffd499bd8cedbea28ab23d765c9659d0b8ea4d2c
Alex Nunzi <alex.nunzi@hotmail.it>
2023-06-07 14:34:42 +0200

    Modificato contenuto della mail di notifica di ricezione di una mail di contatto

--- resources/views/emails/new-contact-mail.blade.php
+++ resources/views/emails/new-contact-mail.blade.php
@@ -1,1 +1,12 @@
-prova
+<strong>Ciao amministratore,</strong>
+<br />
+
+hai ricevuto un nuovo contatto. Queste sono i dettagli:
+<br /><br />
+Nome: <em>{{ $lead->name }}</em>
+
+<br /><br />
+Email: <em>{{ $lead->email }}</em>
+
+<br /><br />
+Messaggio: <em>{{ $lead->message }}</em>
