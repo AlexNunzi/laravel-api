6bb2aa2f116f7ef9d6b6333778cefeaed703e861
Alex Nunzi <alex.nunzi@hotmail.it>
2023-06-07 14:15:31 +0200

    Implementazione ricezione mail di contatto tramite API.

---
+++ app/Http/Controllers/Api/LeadController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Mail\NewContact;
+use App\Models\Lead;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
+
+class LeadController extends Controller
+{
+    public function store(Request $request)
+    {
+
+        $data = $request->all();
+
+        $validator = Validator::make(
+            $data,
+            [
+                'name' => 'required',
+                'email' => 'required|email',
+                'message' => 'required'
+            ]
+        );
+
+        if ($validator->fails()) {
+            return response()->json(
+                [
+                    'success' => false,
+                    'errors' => $validator->errors()
+                ]
+            );
+        }
+
+        $lead = new Lead();
+        $lead->fill($data);
+        $lead->save();
+
+        Mail::to('alex.nunzi@hotmail.it')->send(new NewContact($lead));
+
+        return response()->json(
+            [
+                'success' => true
+            ]
+        );
+    }
+}
